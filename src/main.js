a08e2f19ae09df579dcdef8e26511d29caf0e1cd
Maksym Guk <maksim.guk@gmail.com>
2025-03-05 16:52:50 +0200

    add Contacts page, finish restyle for desktop-first

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { initMobileMenu } from './js/mobile-menu.js';
+import { setActivePage } from './js/active-page.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   // Ініціалізація мобільного меню
   initMobileMenu();
+  
+  // Встановлення активної сторінки в меню
+  setActivePage();
 });
