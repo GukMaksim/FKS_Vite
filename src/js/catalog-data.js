a833ef05759133e9e2e9fb6557f6b883485dd3c8
Maksym Guk <maksim.guk@gmail.com>
2025-03-06 14:44:10 +0200

    fixed dynamic images

--- src/js/catalog-data.js
+++ src/js/catalog-data.js
@@ -3,28 +3,28 @@ export const stoneTypes = {
   'granite-ukraine': {
     id: 'type1',
     title: 'Граніт України',
-    image: './img/products/stone/granite-ukraine.jpg',
+    image: '/img/products/stone/granite-ukraine.jpg',
     description:
       'Граніт України - це натуральний камінь з України, який має унікальну природну форму та характеристики.',
     items: [
       {
         id: 'type1-1',
         title: 'Граніт України 1',
-        image: '../img/products/stone/granite-ukraine.jpg',
+        image: '/img/products/stone/granite-ukraine.jpg',
         description:
           'Граніт України - це натуральний камінь з України, який має унікальну природну форму та характеристики.',
       },
       {
         id: 'type1-2',
         title: 'Граніт України 2',
-        image: '../img/products/stone/granite-ukraine.jpg',
+        image: '/img/products/stone/granite-ukraine.jpg',
         description:
           'Граніт України - це натуральний камінь з України, який має унікальну природну форму та характеристики.',
       },
       {
         id: 'type1-3',
         title: 'Граніт України 3',
-        image: '../img/products/stone/granite-ukraine.jpg',
+        image: '/img/products/stone/granite-ukraine.jpg',
         description:
           'Граніт України - це натуральний камінь з України, який має унікальну природну форму та характеристики.',
       },
@@ -33,28 +33,28 @@ export const stoneTypes = {
   granite: {
     id: 'type2',
     title: 'Граніт',
-    image: '../img/products/stone/granite.jpeg',
+    image: '/img/products/stone/granite.jpeg',
     description:
       'Граніт - це натуральний камінь, який має унікальну природну форму та характеристики.',
     items: [
       {
         id: 'type2-1',
         title: 'Граніт 1',
-        image: '../img/products/stone/granite-1.jpg',
+        image: '/img/products/stone/granite-1.jpg',
         description:
           'Граніт - це натуральний камінь, який має унікальну природну форму та характеристики.',
       },
       {
         id: 'type2-2',
         title: 'Граніт 2',
-        image: '../img/products/stone/granite-2.jpg',
+        image: '/img/products/stone/granite-2.jpg',
         description:
           'Граніт - це натуральний камінь, який має унікальну природну форму та характеристики.',
       },
       {
         id: 'type2-3',
         title: 'Граніт 3',
-        image: '../img/products/stone/granite-3.jpg',
+        image: '/img/products/stone/granite-3.jpg',
         description:
           'Граніт - це натуральний камінь, який має унікальну природну форму та характеристики.',
       },
@@ -63,28 +63,28 @@ export const stoneTypes = {
   marble: {
     id: 'type3',
     title: 'Мармур',
-    image: '../img/products/stone/marble.jpg',
+    image: '/img/products/stone/marble.jpg',
     description:
       'Мармур - це натуральний камінь, який має унікальну природну форму та характеристики.',
     items: [
       {
         id: 'type3-1',
         title: 'Мармур 1',
-        image: '../img/products/stone/marble-1.jpg',
+        image: '/img/products/stone/marble-1.jpg',
         description:
           'Мармур - це натуральний камінь, який має унікальну природну форму та характеристики.',
       },
       {
         id: 'type3-2',
         title: 'Мармур 2',
-        image: '../img/products/stone/marble-2.jpg',
+        image: '/img/products/stone/marble-2.jpg',
         description:
           'Мармур - це натуральний камінь, який має унікальну природну форму та характеристики.',
       },
       {
         id: 'type3-3',
         title: 'Мармур 3',
-        image: '../img/products/stone/marble-3.jpg',
+        image: '/img/products/stone/marble-3.jpg',
         description:
           'Мармур - це натуральний камінь, який має унікальну природну форму та характеристики.',
       },
@@ -93,28 +93,28 @@ export const stoneTypes = {
   quartzite: {
     id: 'type4',
     title: 'Кварцит',
-    image: '../img/products/stone/quartzite.jpg',
+    image: '/img/products/stone/quartzite.jpg',
     description:
       'Кварцит - це натуральний камінь, який має унікальну природну форму та характеристики.',
     items: [
       {
         id: 'type4-1',
         title: 'Кварцит 1',
-        image: '../img/products/stone/quartzite.jpg',
+        image: '/img/products/stone/quartzite.jpg',
         description:
           'Кварцит - це натуральний камінь, який має унікальну природну форму та характеристики.',
       },
       {
         id: 'type4-2',
         title: 'Кварцит 2',
-        image: '../img/products/stone/quartzite.jpg',
+        image: '/img/products/stone/quartzite.jpg',
         description:
           'Кварцит - це натуральний камінь, який має унікальну природну форму та характеристики.',
       },
       {
         id: 'type4-3',
         title: 'Кварцит 3',
-        image: '../img/products/stone/quartzite.jpg',
+        image: '/img/products/stone/quartzite.jpg',
         description:
           'Кварцит - це натуральний камінь, який має унікальну природну форму та характеристики.',
       },
@@ -123,28 +123,28 @@ export const stoneTypes = {
   travertine: {
     id: 'type5',
     title: 'Травертин',
-    image: '../img/products/stone/travertine.jpg',
+    image: '/img/products/stone/travertine.jpg',
     description:
       'Травертин - це натуральний камінь, який має унікальну природну форму та характеристики.',
     items: [
       {
         id: 'type5-1',
         title: 'Травертин 1',
-        image: '../img/products/stone/travertine.jpg',
+        image: '/img/products/stone/travertine.jpg',
         description:
           'Травертин - це натуральний камінь, який має унікальну природну форму та характеристики.',
       },
       {
         id: 'type5-2',
         title: 'Травертин 2',
-        image: '../img/products/stone/travertine.jpg',
+        image: '/img/products/stone/travertine.jpg',
         description:
           'Травертин - це натуральний камінь, який має унікальну природну форму та характеристики.',
       },
       {
         id: 'type5-3',
         title: 'Травертин 3',
-        image: '../img/products/stone/travertine.jpg',
+        image: '/img/products/stone/travertine.jpg',
         description:
           'Травертин - це натуральний камінь, який має унікальну природну форму та характеристики.',
       },
@@ -153,28 +153,28 @@ export const stoneTypes = {
   onyx: {
     id: 'type6',
     title: 'Онікс',
-    image: '../img/products/stone/onyx.jpg',
+    image: '/img/products/stone/onyx.jpg',
     description:
       'Онікс - це натуральний камінь, який має унікальну природну форму та характеристики.',
     items: [
       {
         id: 'type6-1',
         title: 'Онікс 1',
-        image: '../img/products/stone/onyx.jpg',
+        image: '/img/products/stone/onyx.jpg',
         description:
           'Онікс - це натуральний камінь, який має унікальну природну форму та характеристики.',
       },
       {
         id: 'type6-2',
         title: 'Онікс 2',
-        image: '../img/products/stone/onyx.jpg',
+        image: '/img/products/stone/onyx.jpg',
         description:
           'Онікс - це натуральний камінь, який має унікальну природну форму та характеристики.',
       },
       {
         id: 'type6-3',
         title: 'Онікс 3',
-        image: '../img/products/stone/onyx.jpg',
+        image: '/img/products/stone/onyx.jpg',
         description:
           'Онікс - це натуральний камінь, який має унікальну природну форму та характеристики.',
       },
@@ -183,28 +183,28 @@ export const stoneTypes = {
   sandstone: {
     id: 'type7',
     title: 'Піщаник',
-    image: '../img/products/stone/sandstone.jpg',
+    image: '/img/products/stone/sandstone.jpg',
     description:
       'Піщаник - це натуральний камінь, який має унікальну природну форму та характеристики.',
     items: [
       {
         id: 'type7-1',
         title: 'Піщаник 1',
-        image: '../img/products/stone/sandstone.jpg',
+        image: '/img/products/stone/sandstone.jpg',
         description:
           'Піщаник - це натуральний камінь, який має унікальну природну форму та характеристики.',
       },
       {
         id: 'type7-2',
         title: 'Піщаник 2',
-        image: '../img/products/stone/sandstone.jpg',
+        image: '/img/products/stone/sandstone.jpg',
         description:
           'Піщаник - це натуральний камінь, який має унікальну природну форму та характеристики.',
       },
       {
         id: 'type7-3',
         title: 'Піщаник 3',
-        image: '../img/products/stone/sandstone.jpg',
+        image: '/img/products/stone/sandstone.jpg',
         description:
           'Піщаник - це натуральний камінь, який має унікальну природну форму та характеристики.',
       },
@@ -217,31 +217,31 @@ export const productCategories = {
   countertops: {
     id: 'category1',
     title: 'Стільниці',
-    image: '../img/products/countertop.jpg',
+    image: '/img/products/countertop.jpg',
     description: 'Стільниці з натурального каменю',
     items: [
       {
         id: 'category1-1',
         title: 'Стільниця з граніту',
-        image: '../img/products/countertop.jpg',
+        image: '/img/products/countertop.jpg',
         description: 'Стільниця з граніту',
       },
       {
         id: 'category1-2',
         title: 'Стільниця з мармуру',
-        image: '../img/products/countertop.jpg',
+        image: '/img/products/countertop.jpg',
         description: 'Стільниця з мармуру',
       },
       {
         id: 'category1-3',
         title: 'Стільниця з кварциту',
-        image: '../img/products/countertop.jpg',
+        image: '/img/products/countertop.jpg',
         description: 'Стільниця з кварциту',
       },
       {
         id: 'category1-4',
         title: 'Стільниця з оніксу',
-        image: '../img/products/countertop.jpg',
+        image: '/img/products/countertop.jpg',
         description: 'Стільниця з оніксу',
       },
     ],
@@ -249,31 +249,31 @@ export const productCategories = {
   windowsills: {
     id: 'category2',
     title: 'Підвіконня',
-    image: '../img/products/windowsill.jpg',
+    image: '/img/products/windowsill.jpg',
     description: 'Підвіконня з натурального каменю',
     items: [
       {
         id: 'category2-1',
         title: 'Підвіконня з граніту',
-        image: '../img/products/windowsill.jpg',
+        image: '/img/products/windowsill.jpg',
         description: 'Підвіконня з граніту',
       },
       {
         id: 'category2-2',
         title: 'Підвіконня з мармуру',
-        image: '../img/products/windowsill.jpg',
+        image: '/img/products/windowsill.jpg',
         description: 'Підвіконня з мармуру',
       },
       {
         id: 'category2-3',
         title: 'Підвіконня з травертину',
-        image: '../img/products/windowsill.jpg',
+        image: '/img/products/windowsill.jpg',
         description: 'Підвіконня з травертину',
       },
       {
         id: 'category2-4',
         title: 'Підвіконня з оніксу',
-        image: '../img/products/windowsill.jpg',
+        image: '/img/products/windowsill.jpg',
         description: 'Підвіконня з оніксу',
       },
     ],
@@ -281,19 +281,19 @@ export const productCategories = {
   stairs: {
     id: 'category3',
     title: 'Сходи',
-    image: '../img/products/stairs.jpg',
+    image: '/img/products/stairs.jpg',
     description: 'Сходи з натурального каменю',
     items: [
       {
         id: 'category3-1',
         title: 'Сходи з граніту',
-        image: '../img/products/stairs.jpg',
+        image: '/img/products/stairs.jpg',
         description: 'Сходи з граніту',
       },
       {
         id: 'category3-2',
         title: 'Сходи з мармуру',
-        image: '../img/products/stairs.jpg',
+        image: '/img/products/stairs.jpg',
         description: 'Сходи з мармуру',
       },
     ],
@@ -301,13 +301,13 @@ export const productCategories = {
   fireplaces: {
     id: 'category4',
     title: 'Каміни',
-    image: '../img/products/fireplace.jpg',
+    image: '/img/products/fireplace.jpg',
     description: 'Каміни з натурального каменю',
     items: [
       {
         id: 'category4-1',
         title: 'Каміни',
-        image: '../img/products/fireplace.jpg',
+        image: '/img/products/fireplace.jpg',
         description: 'Каміни',
       },
     ],
@@ -315,19 +315,19 @@ export const productCategories = {
   floors: {
     id: 'category5',
     title: 'Підлоги',
-    image: '../img/products/floor.jpg',
+    image: '/img/products/floor.jpg',
     description: 'Підлоги з натурального каменю',
     items: [
       {
         id: 'category5-1',
         title: 'Підлоги з мармуру',
-        image: '../img/products/floor.jpg',
+        image: '/img/products/floor.jpg',
         description: 'Підлоги з натурульного мармуру',
       },
       {
         id: 'category5-2',
         title: 'Підлоги з граніту',
-        image: '../img/products/floor.jpg',
+        image: '/img/products/floor.jpg',
         description: 'Підлоги з граніту',
       },
     ],
@@ -335,26 +335,26 @@ export const productCategories = {
   bathrooms: {
     id: 'category6',
     title: 'Ванна кімната',
-    image: '../img/products/bathroom.jpg',
+    image: '/img/products/bathroom.jpg',
     description: 'Ванна кімната з натурального каменю',
     items: [{}],
   },
   tiles: {
     id: 'category7',
     title: 'Плитка',
-    image: '../img/products/tiles.jpg',
+    image: '/img/products/tiles.jpg',
     description: 'Плитка з натурального каменю',
     items: [
       {
         id: 'category7-1',
         title: 'Плитка з мармуру',
-        image: '../img/products/tiles.jpg',
+        image: '/img/products/tiles.jpg',
         description: 'Плитка з мармуру',
       },
       {
         id: 'category7-2',
         title: 'Плитка з граніту',
-        image: '../img/products/tiles.jpg',
+        image: '/img/products/tiles.jpg',
         description: 'Плитка з граніту',
       },
     ],
@@ -362,31 +362,31 @@ export const productCategories = {
   monuments: {
     id: 'category8',
     title: "Пам'ятники",
-    image: '../img/products/monuments.jpg',
+    image: '/img/products/monuments.jpg',
     description: "Пам'ятники з натурального каменю",
     items: [
       {
         id: 'category8-1',
         title: "Одинарні пам'ятники",
-        image: '../img/products/monuments.jpg',
+        image: '/img/products/monuments.jpg',
         description: "Одинарні пам'ятники з натурального каменю",
       },
       {
         id: 'category8-2',
         title: "Подвійні пам'ятники",
-        image: '../img/products/monuments.jpg',
+        image: '/img/products/monuments.jpg',
         description: "Подвійні пам'ятники з натурального каменю",
       },
       {
         id: 'category8-3',
         title: 'Меморіальні комплекси',
-        image: '../img/products/monuments.jpg',
+        image: '/img/products/monuments.jpg',
         description: 'Меморіальні комплекси з натурального каменю',
       },
       {
         id: 'category8-4',
         title: "Фотопортрети на пам'ятники",
-        image: '../img/products/monuments.jpg',
+        image: '/img/products/monuments.jpg',
         description: "Фотопортрети на пам'ятники з натурального каменю",
       },
     ],
@@ -394,31 +394,31 @@ export const productCategories = {
   paving: {
     id: 'category9',
     title: 'Бруківка',
-    image: '../img/products/paving.jpg',
+    image: '/img/products/paving.jpg',
     description: 'Бруківка з натурального каменю',
     items: [
       {
         id: 'category9-1',
         title: 'Бруківка пиляна',
-        image: '../img/products/paving.jpg',
+        image: '/img/products/paving.jpg',
         description: 'Бруківка пиляна',
       },
       {
         id: 'category9-2',
         title: 'Бруківка пиляно-колота',
-        image: '../img/products/paving.jpg',
+        image: '/img/products/paving.jpg',
         description: 'Бруківка пиляно-колота',
       },
       {
         id: 'category9-3',
         title: 'Бруківка колота',
-        image: '../img/products/paving.jpg',
+        image: '/img/products/paving.jpg',
         description: 'Бруківка колота',
       },
       {
         id: 'category9-3',
         title: 'Бруківка галтована',
-        image: '../img/products/paving.jpg',
+        image: '/img/products/paving.jpg',
         description: 'Бруківка галтована',
       },
     ],
@@ -426,31 +426,31 @@ export const productCategories = {
   slabs: {
     id: 'category10',
     title: 'Слеби',
-    image: '../img/products/slabs.jpg',
+    image: '/img/products/slabs.jpg',
     description: 'Слеби з натурального каменю',
     items: [
       {
         id: 'category10-1',
         title: 'Слеби з мармуру',
-        image: '../img/products/slabs.jpg',
+        image: '/img/products/slabs.jpg',
         description: 'Слеби з натурального мармуру',
       },
       {
         id: 'category10-2',
         title: 'Слеби з граніту',
-        image: '../img/products/slabs.jpg',
+        image: '/img/products/slabs.jpg',
         description: 'Слеби з граніту',
       },
       {
         id: 'category10-3',
         title: 'Слеби з оніксу',
-        image: '../img/products/slabs.jpg',
+        image: '/img/products/slabs.jpg',
         description: 'Слеби з оніксу',
       },
       {
         id: 'category10-4',
         title: 'Слеби з травертину',
-        image: '../img/products/slabs.jpg',
+        image: '/img/products/slabs.jpg',
         description: 'Слеби з травертину',
       },
     ],
@@ -458,21 +458,21 @@ export const productCategories = {
   rosettes: {
     id: 'category11',
     title: 'Розетки пано',
-    image: '../img/products/rosettes.jpg',
+    image: '/img/products/rosettes.jpg',
     description: 'Розетки пано з натурального каменю',
     items: [{}],
   },
   entrances: {
     id: 'category12',
     title: 'Вхідні групи',
-    image: '../img/products/entrances.jpg',
+    image: '/img/products/entrances.jpg',
     description: 'Вхідні групи з натурального каменю',
     items: [{}],
   },
   church: {
     id: 'category13',
     title: 'Церковні роботи',
-    image: '../img/products/church.jpg',
+    image: '/img/products/church.jpg',
     description: 'Церковні роботи з натурального каменю',
     items: [
       'Церковна робота з мармуру',
